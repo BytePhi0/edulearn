b2baf8e99d56214c4f9160ead7d56f4e4290a725
Theophilus Praise <theophiluspraise67@gmail.com>
2025-07-06 15:27:55 +0100

    fix

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/database');
- // Assuming you're using PostgreSQL now
-// const pool = require('../config/database');
+ const pool = require('../config/database');
 const axios = require('axios');
 const multer = require('multer');
 const path = require('path');
